2d168caf94235bc1390f24fda92b58894f9189aa
Nahal Anjum <nahalanjun004@gmail.com>
2023-10-14 17:06:40 +0500

    Create myBrowser

---
+++ myBrowser.py
@@ -0,0 +1,54 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtWebEngineWidgets import *
+
+
+class MyBrowser(QMainWindow):
+    def __init__(self):
+        self.window = QWidget()
+        self.window.setWindowTitle('MyBrowser')
+
+        self.layout = QVBoxLayout()
+        self.horizontal = QHBoxLayout()
+
+        self.url_bar = QTextEdit()
+        self.url_bar.setMaximumHeight(30)
+
+        self.go_btn = QPushButton("Go")
+        self.go_btn.setMinimumHeight(30)
+
+        self.back_btn = QPushButton("Back")
+        self.back_btn.setMinimumHeight(30)
+
+        self.forward_btn = QPushButton("Forward")
+        self.forward_btn.setMinimumHeight(30)
+
+        self.horizontal.addWidget(self.url_bar)
+        self.horizontal.addWidget(self.go_btn)
+        self.horizontal.addWidget(self.back_btn)
+        self.horizontal.addWidget(self.forward_btn)
+
+        self.browser = QWebEngineView()
+
+        self.go_btn.clicked.connect(lambda: self.navigate(self.url_bar.toPlainText()))
+        self.forward_btn.clicked.connect(self.browser.forward)
+        self.back_btn.clicked.connect(self.browser.back)
+
+        self.layout.addLayout(self.horizontal)
+        self.layout.addWidget(self.browser)
+
+        self.browser.setUrl(QUrl("http://google.com"))
+
+        self.window.setLayout(self.layout)
+        self.window.show()
+
+    def navigate(self, url):
+        if not url.startswith("http"):
+            url = "http://" + url
+            self.url_bar.setText(url)
+        self.browser.setUrl(QUrl(url))
+
+
+app = QApplication([])
+window = MyBrowser()
+app.exec_()
